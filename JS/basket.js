5cf36f60fc53d596df51320dd078793343a7860b
Benoit <benoit.ricard@outlook.fr>
2021-09-22 10:14:41 +0200

    Formulaire du panier et page de confirmation terminées, projet achevé

--- JS/basket.js
+++ JS/basket.js
@@ -4,66 +4,113 @@
 const basketContenu = document.getElementById('basketContenu') // Contenu de tous les objets du panier
 const spanPrice = document.getElementById('totalPrice') // Prix total du panier
 
-// Appel de l'API des Teddies
+// Variables
+let basket = JSON.parse(localStorage.getItem('basket'))
 
-fetch('http://localhost:3000/api/teddies')
-.then(function(res) {
-    if (res.ok) {
-        return res.json();
-    }
-})
-.then(function(value) {
+// Variables du panier
+let basketProduct = '' // Produit du panier
+let totalPrice = 0 // Prix total du panier
 
-    // Variables
-    let basket = JSON.parse(localStorage.getItem('basket'))
-
-    // Variables du panier
-    let basketProduct = '' // Produit du panier
-    let totalPrice = 0 // Prix total du panier
-
-    // Boucle pour créer les cartes produit dans le panier
-    for(let i = 0; i < basket.length; i++) { 
-        basketProduct =
-            `<div class="basketProduct" id="basketProduct">
-                <div class="basketProductLeft">
-                    <img class="basketProductPhoto" src="${basket[i].imageUrl}">
-                    <div class="basketProductInfo">
-                        <p class="itemProductName">${basket[i].name}</p>
-                        <p class="productId">ID: ${basket[i]._id}</p>
-                        <p class="basketProductColor">Color:</p>
-                    </div>
+// Boucle pour créer les cartes produit dans le panier
+for(let i = 0; i < basket.length; i++) {
+    basketProduct =
+        `<div class="basketProduct" id="basketProduct">
+            <div class="basketProductLeft">
+                <img class="basketProductPhoto" src="${basket[i].imageUrl}">
+                <div class="basketProductInfo">
+                    <p class="itemProductName">${basket[i].name}</p>
+                    <p class="productId">ID: ${basket[i]._id}</p>
+                    <p class="basketProductColor">Color:</p>
                 </div>
-                <div class="basketProductRight">
-                    <p class="productPrice">${basket[i].price}€</p>
-                    <button class="removeButton"><i class="fas fa-trash-alt"></i></button>
-                </div>
-            </div>`
+            </div>
+            <div class="basketProductRight">
+                <p class="productPrice">${basket[i].price}€</p>
+                <button class="removeButton"><i class="fas fa-trash-alt"></i></button>
+            </div>
+        </div>`
 
-        basketContenu.innerHTML += basketProduct
+    basketContenu.innerHTML += basketProduct
 
-        const remove = document.getElementById(`remove${i}`)
+    const remove = document.getElementById(`remove${i}`)
 
-        totalPrice += basket[i].price
+    totalPrice += basket[i].price
+}
+
+// Boutons de suppression
+const buttons = document.getElementsByClassName('removeButton')
+
+// Boucle pour créer les boutons sur chaque produit du panier
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        basket.splice(i, 1)
+        localStorage.setItem('basket', JSON.stringify(basket))
+        location.reload()
+    })
+}
+
+// Prix total du panier
+spanPrice.textContent = `${totalPrice}€`
+
+
+// Formulaire
+const confirm = document.getElementById('submitButton')
+const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/ // Vérifie la conformité de l'adresse mail
+
+// Bouton Submit
+confirm.addEventListener('click', function(event) {
+
+    // Objet contact du formulaire
+    let contact = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        address: document.getElementById('address').value,
+        city: document.getElementById('city').value,
+        email: document.getElementById('email').value
     }
 
+    // Vérification des données du formulaire
+    if (
+        (contact.firstName) &&
+        (contact.lastName) &&
+        (contact.address) &&
+        (contact.city) &&
+        (regexMail.test(contact.email) == true)
+    ) {
 
-    // Boutons de suppression
-    const buttons = document.getElementsByClassName('removeButton')
+        // Informations du panier validé
+        let products = [];
+        for (let i = 0; i < basket.length; i++) {
+            products.push(basket[i]._id)
+        }
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function () {
-            basket.splice(i, 1)
-            localStorage.setItem('basket', JSON.stringify(basket))
-            location.reload()
+        console.log(contact)
+        console.table(products)
+
+        // Envoi des données
+        fetch('http://localhost:3000/api/teddies/order', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                contact,
+                products
+            }),
         })
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(value) {
+            localStorage.setItem('order', JSON.stringify(value))
+            document.location.href = 'confirmation.html'
+        })
+        .catch(function(error) {
+            console.error(error)
+        })
+
+    } else {
+        alert(`Le formulaire n'est pas correctement rempli`)
     }
-
-    // Prix total du panier
-    spanPrice.textContent = `${totalPrice}€`
-
-
 })
-.catch(function(error) {
-});
-
-// Fin de l'appel de l'API des Teddies
--- JS/confirmation.js
+++ JS/confirmation.js
@@ -1,1 +1,30 @@
 'use strict'
+
+// On récupère les éléments du Local Storage
+let basket = JSON.parse(localStorage.getItem('basket'))
+let order = JSON.parse(localStorage.getItem('order'))
+
+// On cible les éléments HTML à remplir
+const thanksName = document.getElementById('thanksName')
+const namesConfirmation = document.getElementById('namesConfirmation')
+const addressConfirmation = document.getElementById('addressConfirmation')
+const emailConfirmation = document.getElementById('emailConfirmation')
+const priceConfirmation = document.getElementById('priceConfirmation')
+const idConfirmation = document.getElementById('idConfirmation')
+
+// On remplit les éléments HTML avec le contenu de 'contact'
+thanksName.textContent = order.contact.firstName
+namesConfirmation.textContent = order.contact.firstName + ' ' + order.contact.lastName
+addressConfirmation.textContent = order.contact.address + ', ' + order.contact.city
+emailConfirmation.textContent = order.contact.email
+
+// On récupère le prix total du panier
+let totalPrice = 0
+
+for (let i = 0; i < basket.length; i++) {
+    totalPrice += basket[i].price
+}
+
+// On affiche le prix total et l'ID fourni par l'API
+priceConfirmation.textContent = totalPrice + '€'
+idConfirmation.textContent = order.orderId
